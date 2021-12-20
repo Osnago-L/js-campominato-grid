1227a41c0d2ca841fcf46c9cb26c1e3d43309e87
Osnago-L <osna.brera@gmail.com>
2021-12-20 18:56:40 +0100

    end project

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,30 +1,31 @@
 function difficultyStart(){
     let difficulty = document.getElementById("difficulty").value;
-    let arrayItem = [];
     if(difficulty == "easy"){
         document.querySelector(".maingrid").innerHTML = "";
         for (let i = 1; i <= 100; i++) {
-            let square = `<div class="grid10"><h1 class="numbox">${i}</h1></div>`;
+            let square = `<div class="grid10 item"><h1 class="numbox">${i}</h1></div>`;
             document.querySelector(".maingrid").innerHTML += square;
-            arrayItem.push(square);
         }
     }else if(difficulty == "medium"){
         document.querySelector(".maingrid").innerHTML = "";
         for (let i = 1; i <= 81; i++) {
-            let square = `<div class="grid9"><h1 class="numbox">${i}</h1></div>`;
+            let square = `<div class="grid9 item"><h1 class="numbox">${i}</h1></div>`;
             document.querySelector(".maingrid").innerHTML += square;
-            arrayItem.push(square);
         }
     }else{
         document.querySelector(".maingrid").innerHTML = "";
         for (let i = 1; i <= 49; i++) {
-            let square = `<div class="grid7"><h1 class="numbox">${i}</h1></div>`;
+            let square = `<div class="grid7 item"><h1 class="numbox">${i}</h1></div>`;
             document.querySelector(".maingrid").innerHTML += square;
-            arrayItem.push(square);
         }
     }
-    console.log(arrayItem)
+        const arrayItem = document.querySelectorAll(".item");
+    
+        for(let i = 0; i < arrayItem.length; i++) {
+            arrayItem[i].addEventListener("click", function() {
+                this.style.backgroundColor = "#6495ED";
+            });
+        }
 
-    for (let i = 0; i < array.length; i++) {
-    }
+    
 }
