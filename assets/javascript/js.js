b4b0f0ff502795aca5076fe172494aa745553642
Osnago-L <osna.brera@gmail.com>
2021-12-20 18:00:27 +0100

    innerhtml instead of createElement

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,42 +1,30 @@
 function difficultyStart(){
     let difficulty = document.getElementById("difficulty").value;
+    let arrayItem = [];
     if(difficulty == "easy"){
         document.querySelector(".maingrid").innerHTML = "";
-        let square = ""
         for (let i = 1; i <= 100; i++) {
-             square = document.createElement("div");
-            let num = document.createElement("h1");
-            document.querySelector(".maingrid").append(square);
-            square.append(num)
-            square.classList.add("grid10");
-            square.classList.add('item-'+i);
-            num.classList.add("numbox")
-            num.innerHTML = i;
+            let square = `<div class="grid10"><h1 class="numbox">${i}</h1></div>`;
+            document.querySelector(".maingrid").innerHTML += square;
+            arrayItem.push(square);
         }
     }else if(difficulty == "medium"){
         document.querySelector(".maingrid").innerHTML = "";
         for (let i = 1; i <= 81; i++) {
-            let square = document.createElement("div");
-            let num = document.createElement("h1");
-            document.querySelector(".maingrid").append(square);
-            square.append(num)
-            square.classList.add("grid9");
-            square.classList.add('item-'+i);
-            num.classList.add("numbox")
-            num.innerHTML = i;
+            let square = `<div class="grid9"><h1 class="numbox">${i}</h1></div>`;
+            document.querySelector(".maingrid").innerHTML += square;
+            arrayItem.push(square);
         }
     }else{
         document.querySelector(".maingrid").innerHTML = "";
         for (let i = 1; i <= 49; i++) {
-            let square = document.createElement("div");
-            let num = document.createElement("h1");
-            document.querySelector(".maingrid").append(square);
-            square.append(num)
-            square.classList.add("grid7");
-            square.classList.add('item-'+i);
-            num.classList.add("numbox")
-            num.innerHTML = i;
+            let square = `<div class="grid7"><h1 class="numbox">${i}</h1></div>`;
+            document.querySelector(".maingrid").innerHTML += square;
+            arrayItem.push(square);
         }
     }
-}
+    console.log(arrayItem)
 
+    for (let i = 0; i < array.length; i++) {
+    }
+}
